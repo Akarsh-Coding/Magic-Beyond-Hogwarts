d4e53c2fed956194bfe88f22ae7fa4bcfac03300
Akarsh-Coding <akarsh4.pvt@gmail.com>
2026-02-02 20:54:37 +0530

    Finish header and navigation bar layout and styling

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let lastScrollY = 0;
 const scrollLimit = 45;
 let header = document.querySelector(".header");
+let navbar = document.querySelector(".navbar");
+let searchImg = document.querySelector(".Search-img");
+let searchInput = document.querySelector(".Search-input");
+let searchBtn = document.querySelector(".Search-btn");
 
 window.addEventListener("scroll", () => {
     const current = window.scrollY;
@@ -13,5 +17,18 @@ window.addEventListener("scroll", () => {
         header.style.transform = "translateY(0)";
     }
 
+    if (header.style.transform == "translateY(-100%)") {
+        navbar.style.display = "grid";
+        // console.dir(navbar)
+    } else {
+        navbar.style.display = "none";
+    }
+
     lastScroll = current;
 });
+
+window.addEventListener("click", () => {
+    searchImg.style.display = "none"
+    searchInput.style.transform = "translateY(0)"
+    searchBtn.style.transform = "translateY(0)"
+})
